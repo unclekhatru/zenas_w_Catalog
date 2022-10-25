fed688be72ea6d19ce885863a21dcec9007c1b5e
Devendra Khatri <116125046+unclekhatru@users.noreply.github.com>
2022-10-25 11:23:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # run a snowflake query and put it all in a var called my_catalog
 my_cur.execute("use role ACCOUNTADMIN ;")
-my_cur.execute("select color_or_style from catalog_for_website")
+my_cur.execute("select color_or_style from catalog_for_website ;")
 my_catalog = my_cur.fetchall()
 # put the dafta into a dataframe
 df = pandas.DataFrame(my_catalog)
